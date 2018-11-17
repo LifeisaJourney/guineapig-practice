70afdd0277693e5b61e25fc2cddffdd9e6ba98bc
LifeH <hwie.zhu@gmail.com>
2018-11-17 17:14:46 -0500

    changing react presentational component into stateless functional component

--- files/components/GuineaPigs.js
+++ files/components/GuineaPigs.js
@@ -1,15 +1,30 @@
+// import React from 'react';
+// export class GuineaPigs extends React.Component {
+//   render() {
+//     const src = this.props.src;
+//     return (
+//       <div>
+//         <h1>Cute Guinea Pigs</h1>
+//         <img src={src} />
+//       </div>
+//     );
+//   }
+// }
+
+////////////////////////Above this line is React Component, normal way of doing a stateless react component.
+
+
+
+///Below this line is the way to write stateless functional component while passing props in and removing this binding and removing render()
+
 import React from 'react';
 
-export class GuineaPigs extends React.Component {
-  
-  render() {
-    const src = this.props.src;
-    
-    return (
-      <div>
-        <h1>Cute Guinea Pigs</h1>
-        <img src={src} />
-      </div>
-    );
-  }
+export const GuineaPigs = (props) => {
+  let src = props.src;
+  return (
+    <div>
+      <h1>Cute Guinea Pigs</h1>
+      <img src = { src } />
+    </div>
+  );
 }
